e65d421a363af88aad590d853a589d0cacb1c3e7
EnPei_Lo <EnPeiLo@chailease.com.tw>
2022-08-04 10:48:40 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
 
 });
 
-var validate = $("#myForm").validate({
+$("#myForm").validate({
+  onkeyup: function (element) {
+    //去除空格
+    var value = this.elementValue(element).replace(/ /g, "");
+    $(element).val(value);
+  },
   rules: {
     NAME: {
       required: true,
@@ -56,39 +61,54 @@ var validate = $("#myForm").validate({
   // }
 });
 
-
+// 日期檢核(yyyyMMdd)
 jQuery.validator.addMethod("yyyyMMdd", function (value, element) {
-  var dateString = '';
-  dateString = value.slice(0, 4) + '-' + value.slice(4, 6) + '-' + value.slice(-2);
-  var dateValid = new Date(dateString).toString() !== 'Invalid Date';
-  return this.optional(element) || dateValid;
+  return this.optional(element) || isDate8(value);
 }, "日期格式錯誤");
-
+// 手機格式檢合
 jQuery.validator.addMethod("mobile", function (value, element) {
   var mobileValid = new RegExp(/^09\d{8}$/).test(value);
   return this.optional(element) || mobileValid;
 }, "手機格式錯誤");
-
+// 身分證格式檢核
 jQuery.validator.addMethod("id", function (value, element) {
   return this.optional(element) || taiwanIdValidator.isNationalIdentificationNumberValid(value);
 }, "身分證格式錯誤");
-
+// 統一編號格式檢核
 jQuery.validator.addMethod("gui", function (value, element) {
   return this.optional(element) || taiwanIdValidator.isGuiNumberValid(value);
 }, "統一編號格式錯誤");
 
-jQuery.validator.addMethod("idGui", function (value, element,params) {
+// 身分證/統編格式檢核
+jQuery.validator.addMethod("idGui", function (value, element, params) {
   var word1 = value.substring(0, 1);
   var isId = new RegExp("[A-Za-z]+").test(word1);
   var isGui = new RegExp(/[0-9]/).test(word1);
-  if(isId){
+  if (isId) {
     return this.optional(element) || taiwanIdValidator.isNationalIdentificationNumberValid(value);
   }
-  if(isGui){
+  if (isGui) {
     return this.optional(element) || taiwanIdValidator.isGuiNumberValid(value);
   }
-  
+
 }, '身分證/統編格式錯誤');
+
+
+function isDate8(sDate) {
+  if (!/^[0-9]{8}$/.test(sDate)) {
+    return false;
+  }
+  var year, month, day;
+  year = sDate.substring(0, 4);
+  month = sDate.substring(4, 6);
+  day = sDate.substring(6, 8);
+  var iaMonthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if (year < 1700 || year > 2500) return false
+  if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) iaMonthDays[1] = 29;
+  if (month < 1 || month > 12) return false
+  if (day < 1 || day > iaMonthDays[month - 1]) return false
+  return true
+}
 
 
 
